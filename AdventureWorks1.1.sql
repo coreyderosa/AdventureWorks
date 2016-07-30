668fb8cdd1f0c5d75ad1b6bd690640b6a05a3ef2
coreyderosa <coreyderosa@gmail.com>
2016-07-29 19:45:35 -0600

    Multiple parameters
    Added a parameter to proc

--- AdventureWorks1.1.sql
+++ AdventureWorks1.1.sql
@@ -1,10 +1,11 @@
 USE AdventureWorks2012
 GO
 
-ALTER PROC dbo.uspGetAddress @City nvarchar(30)
+ALTER PROC dbo.uspGetAddress @City nvarchar(30), @AddressLine1 nvarchar(60)
 AS
 
 SELECT * 
 FROM Person.Address
 WHERE City LIKE @City + '%'
+AND AddressLine1 LIKE @AddressLine1 + '%'
 GO
